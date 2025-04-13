0b1d55c9138a095753ecdd926a85d331df23ee0e
spidernet <95533522+boazglick@users.noreply.github.com>
2025-04-13 20:04:08 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteUrl: "https://giffer.co.il", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://digpcola.com", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "giffer", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "digpcola", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "giffer", // This allows an alternative site description for meta data for pages.
+    "digpcola", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "giffer", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "digpcola", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
   subscribeWidget: {
     visible: false,
-    title: "giffer",
+    title: "digpcola",
     helpText: "הישארו מעודכנים",
     successMessage: "תודה ההודעה נשלחה בהצלחה",
   },
@@ -25,11 +25,11 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://giffer.co.il/",
+        url: "https://digpcola.com/",
       },
       {
         label: "יצירת קשר",
-        url: "https://giffer.co.il/contact",
+        url: "https://digpcola.com/contact",
       },
     ],
   },
@@ -38,16 +38,16 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://giffer.co.il/",
+        url: "https://digpcola.com/",
       },
       {
         label: "Sitemap",
-        url: "https://giffer.co.il/sitemap.xml",
+        url: "https://digpcola.com/sitemap.xml",
       },
       
       {
         label: "יצירת קשר",
-        url: "https://giffer.co.il/contact",
+        url: "https://digpcola.com/contact",
       },
       
      
@@ -71,9 +71,9 @@ module.exports = {
     successMessage: "We’ll get in touch with you soon.",
   },
   metadata: {
-    title: "giffer.co.il",
+    title: "digpcola.com",
     description:
-      "giffer.co.il",
+      "digpcola.com",
   },
   twitterCard: {
     title: "",
@@ -89,9 +89,9 @@ module.exports = {
     imageUrl: "facebookImage.png",
     appId: "2391725224459953",
   },
-  siteTitle: "giffer.co.il",
+  siteTitle: "digpcola.com",
   siteDescription:
-    "giffer.co.il",
+    "digpcola.com",
   language: "he",
   logoUrl: "",
   iconUrl: "",
